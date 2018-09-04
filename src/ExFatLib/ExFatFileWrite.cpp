c8c9ea6d99bc0bab38515dfa5c5cddd6ee87047c
Bill Greiman <fat16lib@sbcglobal.net>
2018-09-04 07:58:31 -0700

    Fix mkdir

--- src/ExFatLib/ExFatFileWrite.cpp
+++ src/ExFatLib/ExFatFileWrite.cpp
@@ -222,6 +222,7 @@ bool ExFatFile::mkdir(ExFatFile* parent, ExName_t* fname) {
   // Set to start of dir
   rewind();
   m_flags = FILE_FLAG_READ | FILE_FLAG_CONTIGUOUS | FILE_FLAG_DIR_DIRTY;
+  return sync();
 
 fail:
   return false;
